1e565f505b2b67d53073522fb9df0ec0b8a297ae
Maxim Razin <blvckonej@gmail.com>
2023-01-14 21:53:42 +0300

    added users in db

---
+++ src/components/StatBlock/StatBlock.jsx
@@ -0,0 +1,21 @@
+import { React,  useEffect,  useState } from 'react'
+
+
+import './StatBlock'
+
+const StatBlock = () => {
+
+    render (
+        <div className="container">
+            <div className="header-block">
+                <h2>Главная</h2>
+            </div>
+            <div className="statistic-block">
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default StatBlock
